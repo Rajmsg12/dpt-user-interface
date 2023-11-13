0553fd67c4cc9cae72ed6cb601d284ea16b53beb
AkashChoudharyMSG <akashchoudhary.msg@gmail.com>
2023-11-13 14:34:11 +0530

    file added

--- src/components/TourPage/Itinerary.js
+++ src/components/TourPage/Itinerary.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Itinerary = () => {
+    const [itineraryData, setItineraryData] = useState([]);
+    const url = window.location.href;
+    const spliturl = url.split("/");
+    const slug = spliturl[4];
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://127.0.0.1:9900/${slug}`);
+                const data = await response.json();
+                if (data.status === 'success' && data.data.length > 0) {
+                    setItineraryData(data.data[0].itinerary_info);
+                } else {
+                    console.error('Failed to fetch itinerary data');
+                }
+            } catch (error) {
+                console.error('Error fetching itinerary data:', error);
+            }
+        };
+
+        fetchData();
+    }, []); // Empty dependency array to fetch data only once when the component mounts
+
     return (
         <div>
             <div className="Itinerary">
@@ -8,91 +30,50 @@ const Itinerary = () => {
                 <div className="ItineraryIn">
                     <div className="d-flex align-items-start">
                         <div className="nav flex-column nav-pills LhsItineraryDiv" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                            <button className="nav-link active" id="v-pills-1tinerary1-tab" data-bs-toggle="pill" data-bs-target="#v-pills-1tinerary1" type="button" role="tab" aria-controls="v-pills-1tinerary1" aria-selected="true">
-                                <div className="parentWrapper">
-                                    <div className="imgWrapper">
-                                        <img src={"https://res.cloudinary.com/dqslvlm0d/image/upload/v1697702484/itinerary1_jl499i.jpg"} alt="" />
+                            {itineraryData.map((item, index) => (
+                                <button
+                                    key={index}
+                                    className={`nav-link ${index === 0 ? 'active' : ''}`}
+                                    id={`v-pills-itinerary${index + 1}-tab`}
+                                    data-bs-toggle="pill"
+                                    data-bs-target={`#v-pills-itinerary${index + 1}`}
+                                    type="button"
+                                    role="tab"
+                                    aria-controls={`v-pills-itinerary${index + 1}`}
+                                    aria-selected={index === 0 ? 'true' : 'false'}
+                                >
+                                    <div className="parentWrapper">
+                                        <div className="imgWrapper">
+                                            <img src={`http://127.0.0.1:8800/data/uploads/${item.image}`} alt="" />
+                                        </div>
+                                        <div className="Text">
+                                            <div className="title">{item.name}</div>
+                                            <span className="description">{item.description}</span>
+                                        </div>
                                     </div>
-                                    {/* imgWrapper */}
-                                    <div className="Text">
-                                        <div className="title">Burj Al Arab | Dubai's Most Iconic Hotel</div>
-                                        <span className="description">Welcome to Burj Al Arab, Dubai's most iconic hotel and a global icon of Arabian luxury.</span>
-                                    </div>
-                                    {/* Text */}
-                                </div>
-                                {/* parentWrapper */}
-                            </button>
-                            <button className="nav-link" id="v-pills-1tinerary2-tab" data-bs-toggle="pill" data-bs-target="#v-pills-1tinerary2" type="button" role="tab" aria-controls="v-pills-1tinerary2" aria-selected="false">
-                                <div className="parentWrapper">
-                                    <div className="imgWrapper">
-                                        <img src={"https://res.cloudinary.com/dqslvlm0d/image/upload/v1697702485/itinerary2_nzvdd4.jpg"} alt="" />
-                                    </div>
-                                    {/* imgWrapper */}
-                                    <div className="Text">
-                                        <div className="title">10 Amazing Inside Burj Al Arab Secrets</div>
-                                        <span className="description">60 minutes open desert ATV Quad biking and another 30 minutes high dunes roller ride.</span>
-                                    </div>
-                                    {/* Text */}
-                                </div>
-                                {/* parentWrapper */}
-                            </button>
-                            <button className="nav-link" id="v-pills-1tinerary3-tab" data-bs-toggle="pill" data-bs-target="#v-pills-1tinerary3" type="button" role="tab" aria-controls="v-pills-1tinerary3" aria-selected="false">
-                                <div className="parentWrapper">
-                                    <div className="imgWrapper">
-                                        <img src={"https://res.cloudinary.com/dqslvlm0d/image/upload/v1697702485/itinerary3_kfk2oi.jpg"} alt="" />
-                                    </div>
-                                    {/* imgWrapper */}
-                                    <div className="Text">
-                                        <div className="title"> Burj Al Arab Tour opening five-star hotel</div>
-                                        <span className="description">Welcome to Burj Al Arab, Dubai's most iconic hotel and a global icon of Arabian luxury.</span>
-                                    </div>
-                                    {/* Text */}
-                                </div>
-                                {/* parentWrapper */}
-                            </button>
-                            <button className="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">
-                                <div className="parentWrapper">
-                                    <div className="imgWrapper">
-                                        <img src={"https://res.cloudinary.com/dqslvlm0d/image/upload/v1697702486/itinerary4_y0xwlt.jpg"} alt="" />
-                                    </div>
-                                    {/* imgWrapper */}
-                                    <div className="Text">
-                                        <div className="title"> Inside Burj Al Arab, Dubai - Hotel Interior Design</div>
-                                        <span className="description">60 minutes open desert ATV Quad biking and another 30 minutes high dunes roller ride.</span>
-                                    </div>
-                                    {/* Text */}
-                                </div>
-                                {/* parentWrapper */}
-                            </button>
+                                </button>
+                            ))}
                         </div>
                         <div className="tab-content RhsItineraryDiv" id="v-pills-tabContent">
-                            <div className="tab-pane fade show active" id="v-pills-1tinerary1" role="tabpanel" aria-labelledby="v-pills-1tinerary1-tab">
-                                <div className="TabImg1">
-                                    <img src={"https://res.cloudinary.com/dqslvlm0d/image/upload/v1697702484/image1_r79ie6.jpg"} alt="" />
+                            {itineraryData.map((item, index) => (
+                                <div
+                                    key={index}
+                                    className={`tab-pane fade ${index === 0 ? 'show active' : ''}`}
+                                    id={`v-pills-itinerary${index + 1}`}
+                                    role="tabpanel"
+                                    aria-labelledby={`v-pills-itinerary${index + 1}-tab`}
+                                >
+                                    <div className="TabImg1">
+                                        <img src={`http://127.0.0.1:8800/data/uploads/${item.image}`} alt="" />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="tab-pane fade" id="v-pills-1tinerary2" role="tabpanel" aria-labelledby="v-pills-1tinerary2-tab">
-                                <div className="TabImg1">
-                                    <img src={"https://res.cloudinary.com/dqslvlm0d/image/upload/v1697702484/image1_r79ie6.jpg"} alt="" />
-                                </div>
-                            </div>
-                            <div className="tab-pane fade" id="v-pills-1tinerary3" role="tabpanel" aria-labelledby="v-pills-1tinerary3-tab">
-                                <div className="TabImg1">
-                                    <img src={"https://res.cloudinary.com/dqslvlm0d/image/upload/v1697702484/image1_r79ie6.jpg"} alt="" />
-                                </div>
-                            </div>
-                            <div className="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
-                                <div className="TabImg1">
-                                    <img src={"https://res.cloudinary.com/dqslvlm0d/image/upload/v1697702484/image1_r79ie6.jpg"} alt="" />
-                                </div>
-                            </div>
+                            ))}
                         </div>
                     </div>
                 </div>
-                {/* ItineraryIn */}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Itinerary
+export default Itinerary;
