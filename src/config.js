45f2f662fe2282ffea1417d26cfe12a9ff1a3b9d
AkashChoudharyMSG <akashchoudhary.msg@gmail.com>
2023-11-23 19:03:39 +0530

    file added

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // config.js
 const config = {
     baseUrl: 'http://127.0.0.1:9900',
-    imageUrl:'https://phpstack-1167113-4078182.cloudwaysapps.com/backendapi/data/uploads'
+    imageUrl:'http://127.0.0.1:8800/data/uploads'
   };
   
   export default config;
