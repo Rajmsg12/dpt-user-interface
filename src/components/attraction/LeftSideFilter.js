ea68696207acf477b5c390598dd81524535d5d95
AkashChoudharyMSG <akashchoudhary.msg@gmail.com>
2024-01-05 14:51:44 +0530

    category slug

--- src/components/attraction/LeftSideFilter.js
+++ src/components/attraction/LeftSideFilter.js
@@ -85,12 +85,12 @@ const LeftSideFilter = ({
                 {showAllCategories
                   ? categories.map((item, index) => (
                     <li key={index}>
-                      <Link to={`/${item.name?.toLowerCase().replace(/\s+/g, '-')}`}>{item.name}</Link>
+                      <Link to={`/${item.slug}`}>{item.name}</Link>
                     </li>
                   ))
                   : categories.slice(0, 8).map((item, index) => (
                     <li key={index}>
-                      <Link to={`/${item.name?.toLowerCase().replace(/\s+/g, '-')}`}>{item.name}</Link>
+                      <Link to={`/${item.slug}`}>{item.name}</Link>
                     </li>
                   ))}
               </ul>
