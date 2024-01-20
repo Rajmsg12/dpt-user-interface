d2742d0a9c71248788fda7ab1d38bb92d42792a1
AkashChoudharyMSG <akashchoudhary.msg@gmail.com>
2024-01-20 15:58:15 +0530

    file added

--- src/components/404/notFound.js
+++ src/components/404/notFound.js
@@ -13,10 +13,12 @@ const NotFound = () => {
       <InnerHeader/>
       <div className="notfoundpage">
       <div className="container">
-            <h1>404</h1>
+            <div className="404image">
+            <img src={"https://res.cloudinary.com/debhpxgxy/image/upload/v1703744960/tftjulewlapazwqf3wjr.png"} alt="" />
+            </div>
             <h2>Page not found</h2>
             <p>The page you are looking for does not exist.</p>
-            <button className="cta" onClick={() => navigate('/')}>BACK TO HOME</button>
+            <button className="cta" onClick={() => navigate('/')}>Back to home</button>
       </div>
       </div>
       <Footer/>
